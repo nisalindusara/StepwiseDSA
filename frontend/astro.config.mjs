61f244e7d9a7def11359136e1351a0f9e8fe1c83
Nisal <91654417+nisalindusara@users.noreply.github.com>
2026-09-04 19:08:53 +0530

    feat: Create blog post layout

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,9 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import icon from 'astro-icon';
+import icon from "astro-icon";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()]
+  integrations: [icon(), mdx()],
+  markdown: {
+    shikiConfig: {
+      theme: "css-variables",
+    },
+  },
 });
